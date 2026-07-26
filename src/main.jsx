7aab225dc9f4dd16fa17e945c19a08ad716d2415
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-26 22:23:28 +0500

    Load simplified Flowcordia source editor styling

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import "./studio.css";
 import "./fixes.css";
 import "./reactflow-monaco.css";
 import "./n8n-workbench.css";
+import "./source-editor.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
