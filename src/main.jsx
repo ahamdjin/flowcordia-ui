639645bf3694cd59b87771c9776ff7b6c4367d0e
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-26 12:03:33 +0500

    Load n8n canvas and Monaco workbench styling

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import "./styles.css";
 import "./studio.css";
 import "./fixes.css";
 import "./reactflow-monaco.css";
+import "./n8n-workbench.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
