14e1cfee0e884f7385e4d0c68b38f0286d3240f9
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-26 11:00:17 +0500

    Load React Flow and Monaco Studio styling

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from "./App";
 import "./styles.css";
 import "./studio.css";
 import "./fixes.css";
+import "./reactflow-monaco.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
