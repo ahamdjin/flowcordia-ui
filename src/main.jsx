490679c39f9653d6a8260eb992e479ddd9767af4
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-26 10:02:53 +0500

    Load final Vercel layout hardening styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./studio.css";
+import "./fixes.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
